be242b5311b26c2ba68625359181f4f52910e236
db <91306375+db-dao@users.noreply.github.com>
2024-04-06 18:14:26 -0400

    fix

--- app/api/luma/route.ts
+++ app/api/luma/route.ts
@@ -1,22 +1,22 @@
-import { LumaEvent } from "@/types/interfaces";
-import axios from "axios";
+// import { LumaEvent } from "@/types/interfaces";
+// import axios from "axios";
 
-export const getLumaEvent = async (
-    {
-        event_id
-    }: {
-        event_id: string
-    }) => {
+// export const getLumaEvent = async (
+//     {
+//         event_id
+//     }: {
+//         event_id: string
+//     }) => {
 
-    const options = {
-        method: 'GET',
-        url: `https://api.lu.ma/public/v1/event/get?api_id=${event_id}`,
-        headers: {
-            accept: 'application/json',
-            'x-luma-api-key': process.env.LUMA_API_KEY
-        }
-    };
+//     const options = {
+//         method: 'GET',
+//         url: `https://api.lu.ma/public/v1/event/get?api_id=${event_id}`,
+//         headers: {
+//             accept: 'application/json',
+//             'x-luma-api-key': process.env.LUMA_API_KEY
+//         }
+//     };
 
-    const lumaEvent = await axios.request(options)
-    return lumaEvent.data as LumaEvent;
-}
+//     const lumaEvent = await axios.request(options)
+//     return lumaEvent.data as LumaEvent;
+// }
