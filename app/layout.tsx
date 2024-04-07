120eebed83921d23dc4786544313fa22cf447edd
db <91306375+db-dao@users.noreply.github.com>
2024-04-07 12:11:27 -0400

    asdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "DeSciNYC - Decentlized Scinece NYC",
   description: "We are a community of scientists, engineers, and enthusiasts who are passionate about decentralized science.",
+  icons: [
+    {
+      url: "/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png"
+    },
+    {
+      url: "/android-chrome-512x512.png",
+      sizes: "512x512",
+      type: "image/png"
+    }
+  ],
+  themeColor: "#86efac",
 };
 
 export default function RootLayout({
@@ -20,7 +33,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-          {children}
+        {children}
         <Footer />
       </body>
     </html>
