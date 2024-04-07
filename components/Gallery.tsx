f317ffb3a6fb415822c73dc09be0cb71dabef841
db <91306375+db-dao@users.noreply.github.com>
2024-04-07 02:43:04 -0400

    s

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -20,7 +20,7 @@ export default function Gallery() {
             </p>
             <div className="mt-10 flex">
               <a
-                href={links.instagram}
+                href={links.instagram_url}
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 See more Instagram <span aria-hidden="true">&rarr;</span>
