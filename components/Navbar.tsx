95add38b309a682c9fc9f35adabec7ee464f8063
db <91306375+db-dao@users.noreply.github.com>
2024-04-07 12:03:03 -0400

    asdf

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -121,7 +121,9 @@ export default function Navbar() {
               </div>
 
               <div className="-mr-2 flex items-center sm:hidden">
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2
+                text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none 
+                focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   {open ? (
