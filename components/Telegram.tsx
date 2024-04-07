387e5c82368599a7c0a5a54b2dd9b654374c2598
db <91306375+db-dao@users.noreply.github.com>
2024-04-07 02:42:21 -0400

    asdf

--- components/Telegram.tsx
+++ components/Telegram.tsx
@@ -12,7 +12,7 @@ export default function Telegram() {
         <div className="mt-10 flex items-center gap-x-6">
           <a
             target="_blank"
-            href={links.telegram}
+            href={links.telegram_url}
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Join Telegram Group Chat
