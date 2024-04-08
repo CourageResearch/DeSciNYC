ef355ecc769fb1c211bfabe121377a13ae17275a
db <91306375+db-dao@users.noreply.github.com>
2024-04-08 13:07:08 -0400

    asdf

--- components/PastEvents.tsx
+++ components/PastEvents.tsx
@@ -6,7 +6,7 @@ export default function PastEvents() {
         <div className="" id='past-events'>
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <h2 className="text-2xl font-bold tracking-tight text-gray-900">Past Events.</h2>
-                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-12">
+                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-12">
                     {db.slice().reverse().map((event) => {
                         if (event.yt_uuid !== "") {
                             return (
