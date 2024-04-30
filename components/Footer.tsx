35d5a143a7cfbf8c944ea84332852dd71c952310
db <91306375+CourageResearch@users.noreply.github.com>
2024-04-30 11:10:42 -0400

    little change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,6 @@
-'use client'
-
 import { JSX, SVGProps } from "react"
-import { useForm, ValidationError } from '@formspree/react';
 import links from '../links.json'
+import FooterEmailForm from "./FooterEmailForm"
 
 const navigation = {
     events: [
@@ -101,7 +99,6 @@ const navigation = {
 
 export default function Footer() {
 
-    const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORMSPREE_FORM_ID || '');
 
     return (
         <footer className="bg-green-400" aria-labelledby="footer-heading">
@@ -165,48 +162,8 @@ export default function Footer() {
                         </div>
                     </div>
 
-                    <div className="mt-10 xl:mt-0">
-                        <h3 className="text-sm font-semibold leading-6 text-gray-900">Subscribe to the DeSci newsletter</h3>
-                        <p className="mt-2 text-sm leading-6 text-gray-600">
-                            The latest events, news, and resources, sent to your inbox montly.
-                        </p>
-                        <form className="mt-6 sm:flex sm:max-w-md"
+                    <FooterEmailForm/>
 
-                            onSubmit={handleSubmit}
-                        >
-                            <label htmlFor="email-address" className="sr-only">
-                                Email address
-                            </label>
-                            <input
-                                type="email"
-                                name="email-address"
-                                id="email-address"
-                                autoComplete="email"
-                                required
-                                className="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
-                                placeholder="Enter your email"
-                            />
-
-                            <ValidationError
-                                prefix="Email"
-                                field="email"
-                                errors={state.errors}
-                            />
-
-                            <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-                                <button
-                                    disabled={state.submitting}
-                                    type="submit"
-                                    className="rounded-md bg-green-800 px-3.5 py-2.5 text-sm font-semibold text-white 
-                                    shadow-sm hover:bg-green-700 focus-visible:outline focus-visible:outline-2 
-                                    focus-visible:outline-offset-2 focus-visible:outline-green-800"
-                                >
-                                    Subscribe
-                                </button>
-                            </div>
-                        </form>
-                        {state.succeeded && <p className='text-gray-900'>Thanks for subscribing!</p>}
-                    </div>
                 </div>
                 <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
                     <div className="flex space-x-6 md:order-2">
