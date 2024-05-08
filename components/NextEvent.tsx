1d69f0ecf3b3029c3f7dad217684281d9f5f8e8a
db <91306375+CourageResearch@users.noreply.github.com>
2024-05-08 09:47:28 -0400

    asdf

--- components/NextEvent.tsx
+++ components/NextEvent.tsx
@@ -13,12 +13,18 @@ export default function NextEvent(
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                     <div className="lg:pr-8 lg:pt-4">
                         <div className="lg:max-w-lg">
-                            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                            <h2 className="text-base font-semibold leading-7 text-gray-700 mt-4">
                                 Join Us At the Next Event!
+                            </h2>
+
+                            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                                {lumaEvent.name}
                             </p>
+
                             <h2 className="text-base font-semibold leading-7 text-gray-700 mt-4">
                                 {DateTime.fromISO(lumaEvent.start_at).toLocaleString(DateTime.DATETIME_FULL)} in NYC
                             </h2>
+
                             {/* <div className="mt-6 text-lg leading-8 text-gray-900">
                                 {lumaEvent.description.split('More chatting\n', 1)[0].concat('More chatting\n').split('\n').map((line, i) => (
                                     <div key={i}
