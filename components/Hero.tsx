d137f322bd42b10dfa4e17eefb75e159eb7d8a82
db <91306375+CourageResearch@users.noreply.github.com>
2024-07-29 20:12:33 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero(
 ) {
 
     return (
-        <section className="dark:bg-gray-900">
+        <section className="">
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 <div className="mr-auto place-self-center lg:col-span-7">
 
